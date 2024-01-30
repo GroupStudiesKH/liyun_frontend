316abffd9649df874a32a20eef9a3244b16aa69b
mafiapatch <g8071107@gmail.com>
2024-01-30 22:48:04 +0800

    [modify] finish product page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="navbar navbar-expand-lg navbar-light">
     <div class="container">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand" href="/">
         <img src="/assets/img/logo.png" alt="" />
       </a>
       <button
@@ -18,10 +18,10 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="#">企業介紹</a>
+            <a class="nav-link" href="/">企業介紹</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">產品介紹</a>
+            <a class="nav-link" href="product">產品介紹</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">應用產業</a>
--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -31,79 +31,102 @@ export default {
       <img src="/assets/img/product_banner.png" alt="" />
     </div>
 
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-sm-2 category">
-          <div class="title">絕緣材料</div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
-            <li class="list-group-item">A fourth item</li>
-            <li class="list-group-item">And a fifth one</li>
-          </ul>
-
-          <div class="title">絕緣材料</div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
-            <li class="list-group-item">A fourth item</li>
-            <li class="list-group-item">And a fifth one</li>
-          </ul>
-
-          <div class="title">絕緣材料</div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
-            <li class="list-group-item">A fourth item</li>
-            <li class="list-group-item">And a fifth one</li>
-          </ul>
-        </div>
-        <div class="col-12 col-sm-10 list">
+    <div class="gray-background">
+      <div class="white-rounded-background">
+        <div class="container">
           <div class="row">
-            <div class="col-12 route">
-              <span class="material-icons">&#xE88A;</span>首頁 / 產品介紹 /
-              絕緣材料 / NR-INOAC
+            <div class="col-12 col-lg-2 category">
+              <div class="title">絕緣材料</div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">
+                  <a
+                    data-bs-toggle="collapse"
+                    href="#secondMenu"
+                    role="button"
+                    aria-expanded="false"
+                    aria-controls="secondMenu"
+                  >
+                    A second item
+                  </a>
+
+                  <div class="collapse" id="secondMenu">
+                    <ul>
+                      <li>An item</li>
+                      <li>A third item</li>
+                      <li>A fourth item</li>
+                      <li>And a fifth one</li>
+                    </ul>
+                  </div>
+                </li>
+                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">A fourth item</li>
+                <li class="list-group-item">And a fifth one</li>
+              </ul>
+
+              <div class="title">絕緣材料</div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">A fourth item</li>
+                <li class="list-group-item">And a fifth one</li>
+              </ul>
+
+              <div class="title">絕緣材料</div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">A fourth item</li>
+                <li class="list-group-item">And a fifth one</li>
+              </ul>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-6 col-md-3">
-              <img src="/assets/img/product_image.png" />
-              <p>PE熱縮套管</p>
-            </div>
-            <div class="col-6 col-md-3">
-              <img src="/assets/img/product_image.png" />
-              <p>PE熱縮套管</p>
-            </div>
-            <div class="col-6 col-md-3">
-              <img src="/assets/img/product_image.png" />
-              <p>PE熱縮套管</p>
-            </div>
-            <div class="col-6 col-md-3">
-              <img src="/assets/img/product_image.png" />
-              <p>PE熱縮套管</p>
-            </div>
-            <div class="col-6 col-md-3">
-              <img src="/assets/img/product_image.png" />
-              <p>PE熱縮套管</p>
-            </div>
-            <div class="col-6 col-md-3">
-              <img src="/assets/img/product_image.png" />
-              <p>PE熱縮套管</p>
-            </div>
-            <div class="col-6 col-md-3">
-              <img src="/assets/img/product_image.png" />
-              <p>PE熱縮套管</p>
-            </div>
-            <div class="col-6 col-md-3">
-              <img src="/assets/img/product_image.png" />
-              <p>PE熱縮套管</p>
-            </div>
-            <div class="col-6 col-md-3">
-              <img src="/assets/img/product_image.png" />
-              <p>PE熱縮套管</p>
+            <div class="col-12 col-lg-10 list">
+              <div class="row">
+                <div class="col-12 route">
+                  <span class="material-icons">&#xE88A;</span>首頁 / 產品介紹 /
+                  絕緣材料 / NR-INOAC
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-6 col-lg-3">
+                  <img src="/assets/img/product_image.png" />
+                  <p>PE熱縮套管</p>
+                </div>
+                <div class="col-6 col-lg-3">
+                  <img src="/assets/img/product_image.png" />
+                  <p>PE熱縮套管</p>
+                </div>
+                <div class="col-6 col-lg-3">
+                  <img src="/assets/img/product_image.png" />
+                  <p>PE熱縮套管</p>
+                </div>
+                <div class="col-6 col-lg-3">
+                  <img src="/assets/img/product_image.png" />
+                  <p>PE熱縮套管</p>
+                </div>
+                <div class="col-6 col-lg-3">
+                  <img src="/assets/img/product_image.png" />
+                  <p>PE熱縮套管</p>
+                </div>
+                <div class="col-6 col-lg-3">
+                  <img src="/assets/img/product_image.png" />
+                  <p>PE熱縮套管</p>
+                </div>
+                <div class="col-6 col-lg-3">
+                  <img src="/assets/img/product_image.png" />
+                  <p>PE熱縮套管</p>
+                </div>
+                <div class="col-6 col-lg-3">
+                  <img src="/assets/img/product_image.png" />
+                  <p>PE熱縮套管</p>
+                </div>
+                <div class="col-6 col-lg-3">
+                  <img src="/assets/img/product_image.png" />
+                  <p>PE熱縮套管</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
