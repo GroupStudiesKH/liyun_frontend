5adaeb149eb07731e5e7d87176392db144676eba
mafiapatch <g8071107@gmail.com>
2024-02-05 23:26:21 +0800

    [modify] add css animation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script>
 import { useRouter, useRoute } from "vue-router";
+// mounted
+import { onMounted, ref } from "vue";
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 
@@ -10,6 +12,19 @@ export default {
   },
   setup() {
     const router = useRouter();
+    const isIntroScolled = ref(false)
+
+    const handleScroll = () => {
+      // 獲取目標div的相對位置
+
+      const targetDiv = document.getElementById("intro");
+      const rect = targetDiv.getBoundingClientRect();
+
+      // 判斷是否在可視範圍內
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+        isIntroScolled.value = true
+      }
+    }
 
     // const toLastPage = () => {
     //   router.push({ name: "final" });
@@ -20,6 +35,14 @@ export default {
     //   router.push({ name: "intro_2" });
     //   playBackgroundMusic()
     // };
+
+    onMounted(() => {
+      window.addEventListener("scroll", handleScroll);
+    });
+
+    return {
+      isIntroScolled
+    };
   },
 };
 </script>
@@ -49,7 +72,7 @@ export default {
       </div>
     </div>
 
-    <div class="container" id="intro">
+    <div class="container" :class="isIntroScolled ? `scrolled` : ``" id="intro">
       <div class="row">
         <div class="col-6 col-md-3 badge">
           <img src="/assets/img/index_intro_innovative_badge.png" alt="" />
