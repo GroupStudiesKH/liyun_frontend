6a129bc5842897086438748524715a50603c80ea
Chang-Chia Hsu <g8071107@gmail.com>
2024-02-07 14:43:45 +0800

    [modify] update page

--- src/components/ProductMenu.vue
+++ src/components/ProductMenu.vue
@@ -8,11 +8,11 @@
         <div class="collapse col-12 category" id="productMenu">
             <div class="title">絕緣材料</div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">測試中文</li>
+                <li class="list-group-item">導電‧EMI遮蔽材料</li>
                 <li class="list-group-item">
                     <a data-bs-toggle="collapse" href="#secondMenu" role="button" aria-expanded="false"
                         aria-controls="secondMenu">
-                        測試中文
+                        PORON-INOAC
                     </a>
 
                     <div class="collapse" id="secondMenu">
@@ -24,27 +24,37 @@
                         </ul>
                     </div>
                 </li>
-                <li class="list-group-item">測試中文</li>
-                <li class="list-group-item">測試中文</li>
-                <li class="list-group-item">測試中文</li>
+                <li class="list-group-item">CR-INOAC</li>
+                <li class="list-group-item">EPDM-INOAC</li>
+                <li class="list-group-item">NR-INOAC</li>
+                <li class="list-group-item">NBR & Silicone-INOAC</li>
+                <li class="list-group-item">PORON-ROGERS</li>
             </ul>
 
-            <div class="title">絕緣材料</div>
+            <div class="title">泡棉</div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">測試中文</li>
-                <li class="list-group-item">測試中文</li>
-                <li class="list-group-item">測試中文</li>
-                <li class="list-group-item">測試中文</li>
-                <li class="list-group-item">測試中文</li>
+                <li class="list-group-item">矽膠發泡系列</li>
+                <li class="list-group-item">緩衝材料‧UL防火泡棉</li>
+                <li class="list-group-item">導熱‧散熱材料</li>
             </ul>
 
-            <div class="title">絕緣材料</div>
+            <div class="title">膠帶</div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">測試中文</li>
-                <li class="list-group-item">測試中文</li>
-                <li class="list-group-item">測試中文</li>
-                <li class="list-group-item">測試中文</li>
-                <li class="list-group-item">測試中文</li>
+                <li class="list-group-item">絕緣電子膠帶</li>
+                <li class="list-group-item">UL防火‧雙面膠帶</li>
+                <li class="list-group-item">Heat Conduction Material、Heat Dissipation Material</li>
+            </ul>
+
+            <div class="title">其他</div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">シリコーンゴム 導電性シリコーンゴム</li>
+                <li class="list-group-item">熱収縮スリーブ 一般スリーブ</li>
+                <li class="list-group-item">EMI ガスケット、導電性スポンジ</li>
+                <li class="list-group-item">熱縮套管‧一般套管</li>
+                <li class="list-group-item">橡膠系列</li>
+                <li class="list-group-item">銘版‧印刷製品</li>
+                <li class="list-group-item">CNC切断・CNC彫刻</li>
+                
             </ul>
         </div>
 
